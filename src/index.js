5d77d381b42d0f5f9aff63f80db3c67df0120402
Kseniya Tulikava <ksenia77383@gmail.com>
2020-03-02 20:52:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
+module.exports = function reverse (num) {
+  let reversedNumber = 0;
+  let positiveNum = Math.abs(num);
+
+  while (positiveNum != 0) {
+    let lastDigit = positiveNum % 10;
+    reversedNumber = (reversedNumber * 10) + lastDigit;
+    positiveNum = Math.trunc(positiveNum / 10);
+  }
   
+  return reversedNumber;
 }
